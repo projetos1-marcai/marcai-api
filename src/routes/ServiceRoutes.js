34d0e2602516d65cfb37497cc197eba00152d23c
ManoMax <gabrielmaxcontato@gmail.com>
2022-07-02 20:35:03 -0300

    feat: creating new router to create-horario

--- src/routes/ServiceRoutes.js
+++ src/routes/ServiceRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const ServiceController = require('@controllers/ServiceController');
 
 router.get('/services', ServiceController.listServices);
+
+router.post('/service', ServiceController.createService);
+
 router.get('/service-payment-methods', ServiceController.listPaymentMethods);
 router.get('/service-categories', ServiceController.listCategories);
 
-router.post('/service', ServiceController.createService);
+router.post('/horario', ServiceController.createHorario)
 
 module.exports = router;
