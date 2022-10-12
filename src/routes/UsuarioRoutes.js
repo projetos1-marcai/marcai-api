0847d21eb47e1bfd766049024d7378e795307439
ManoMax <gabrielmaxcontato@gmail.com>
2022-10-12 20:20:51 -0300

    refactor: changing structure routes of Usuario

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -5,10 +5,10 @@ const auth = require('../middlewares/Auth')
 router.post('/', UsuarioController.criarUsuario);
 router.post('/login', UsuarioController.login);
 
-router.get('/:id', auth.authorizeUser, UsuarioController.usuarioPorID);
+router.get('/:id_usuario', auth.authorizeUser, UsuarioController.usuarioPorID);
 router.get('/list', auth.authorizeUser, UsuarioController.listarUsuarios);
 
 // Rota destinada para Admin
-router.put('/admin/set-admin/:id', auth.authorizeUser, UsuarioController.setAdmin);
+router.put('/admin/set-admin/:id_usuario', auth.authorizeUser, UsuarioController.setAdmin);
 
 module.exports = router;
--- src/services/UsuarioFunctions/listarUsuarios.js
+++ src/services/UsuarioFunctions/listarUsuarios.js
@@ -2,6 +2,10 @@ const Usuario = require('@models/usuario/Usuario');
 
 async function listarUsuarios(req, res) {
 
+    if (!req.user.admin) {
+        return res.status(404).send({"message": 'Você precisa ter autorização para visualizar a lista de usuários.'});
+    }
+
     const usuarios = await Usuario.find();
 
     return res.status(200).send({"usuarios": usuarios});
