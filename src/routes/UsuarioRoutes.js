b304eec86b866b2365e73b245b4ac65ce311abbf
117211159 LUIGGY FERREIRA DIAS SILVA <luiggy.silva@lcc1-50.lcc.ufcg.edu.br>
2022-10-19 13:17:59 -0300

    feat: adding route to list all fornecedores

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -6,6 +6,7 @@ router.post('/', UsuarioController.criarUsuario);
 router.post('/login', UsuarioController.login);
 
 router.get('/list', auth.authorizeUser, UsuarioController.listarUsuarios);
+router.get('/fornecedores', UsuarioController.listarFornecedores);
 router.get('/:id_usuario', auth.authorizeUser, UsuarioController.usuarioPorID);
 
 // Rota destinada para Admin
