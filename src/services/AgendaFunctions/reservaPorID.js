f2ddaceed548427be50430b8e5dcb3bdbd8a2794
LuiggySilva <luiggy.silva@ccc.ufcg.edu.br>
2022-10-27 22:21:19 -0300

    refact: reservaPorID and reservasUsuario returning dia, servico and id_servico.

--- src/services/AgendaFunctions/reservaPorID.js
+++ src/services/AgendaFunctions/reservaPorID.js
@@ -1,18 +1,24 @@
 const Horario = require('@models/agenda/Horario').Horario;
 const Agenda = require('@models/agenda/Agenda');
+const Servico = require('@models/servico/Servico');
 const Reserva = require('@models/agenda/Reserva').Reserva;
 
 async function reservaPorID(req, res) {
     try {
+
         let reserva = await Reserva.findById({_id: req.params.id_reserva});
         let horario = await Horario.findById({_id: reserva.horario});
         let agenda = await Agenda.findById({_id: horario.agenda});
 
-        if (!req.user.servicos.includes(agenda.servico)) return res.status(404).send({"message": 'O fornecedor não pode recuperar uma reserva de outro fornecedor.'});
+        if (req.user._id.toString() == reserva.cliente || req.user.servicos.includes(agenda.servico)) {
+            let dia_horario = horario.dia;
+            let id_servico = agenda.servico;
+            let servico = await Servico.findById({_id: id_servico})
+            let servico_nome = servico.titulo;
+            return res.status(200).send({"reserva": reserva, "dia":dia_horario, "servico":servico_nome, "id_servico":id_servico});
+        }
         
-        return res.status(200).send({"reserva": reserva});
     } catch (error) {
-        console.log(error);
         return res.status(422).send({"error":"Reserva não encontrada"});
     }
 }
--- src/services/AgendaFunctions/reservasUsuario.js
+++ src/services/AgendaFunctions/reservasUsuario.js
@@ -1,13 +1,30 @@
+const Horario = require('@models/agenda/Horario').Horario;
+const Agenda = require('@models/agenda/Agenda');
+const Servico = require('@models/servico/Servico');
 const Reserva = require('@models/agenda/Reserva').Reserva;
 
 async function reservaPorID(req, res) {
     try {
         let id_usuario = req.user._id.toString()
         let reservas = await Reserva.find({usuario: id_usuario});
-        
-        return res.status(200).send({"reservas": reservas});
+        let reservas_full = []
+
+        for (let index = 0; index < reservas.length; index++) {
+            let reserva = reservas[index]
+            let horario = await Horario.findById({_id: reserva.horario});
+            let agenda = await Agenda.findById({_id: horario.agenda});
+    
+            let dia_horario = horario.dia;
+            let id_servico = agenda.servico;
+            let servico = await Servico.findById({_id: id_servico})
+            let servico_nome = servico.titulo;
+
+            reservas_full.push({"reserva": reserva, "dia":dia_horario, "servico":servico_nome, "id_servico":id_servico})
+        }
+            
+        return res.status(200).send({"reservas": reservas_full});
+    
     } catch (error) {
-        console.log(error);
         return res.status(422).send({"error":error});
     }
 }
